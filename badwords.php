9fcaa0826169ed7254d417644fca7b4c396b7458
Gino804 <giacomo.p03@gmail.com>
2023-07-14 15:13:18 +0200

    Got form data

---
+++ badwords.php
@@ -0,0 +1,3 @@
+<?php
+$_GET['paragraph'];
+$_GET['badword'];
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="badwords.php" method="GET">
+        <label for="paragraph">Paragrafo: </label>
+        <input type='text' name='paragraph' id='paragraph'>
+
+        <label for="badword">Parola da censurare: </label>
+        <input type='text' name='badword' id='badword'>
+
+        <button type="submit">Esegui</button>
+    </form>
+</body>
+
+</html>
