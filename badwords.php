dc440eb9018a226b12dd26c60acec86e2e203adc
Gino804 <giacomo.p03@gmail.com>
2023-07-14 15:57:23 +0200

    Added censor

--- badwords.php
+++ badwords.php
@@ -1,3 +1,28 @@
 <?php
-$_GET['paragraph'];
-$_GET['badword'];
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+$censored_paragraph = str_replace($badword, '***', $paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Paragrafo originale</h3>
+    <p><?= $paragraph ?></p>
+    <p>Lunghezza: <?= strlen($paragraph) ?> caratteri</p>
+
+    <hr>
+    <h3>Paragrafo censurato</h3>
+    <p><?= $censored_paragraph ?></p>
+    <p>Lunghezza: <?= strlen($censored_paragraph) ?></p>
+</body>
+
+</html>
